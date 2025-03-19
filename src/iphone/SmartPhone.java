435ed9822cbcc50b19b46bdf0294b4ea050d20c2
渡辺のぞみ <@192.168.0.15>
2025-03-20 00:49:12 +0900

    Javaオブジェクト指向・継承（演習④完了）

--- src/iphone/SmartPhone.java
+++ src/iphone/SmartPhone.java
@@ -1,6 +1,20 @@
 package iphone;
 
 public class SmartPhone extends Mp3Player {
+	
+	@Override
+	public void play() {
+		System.out.println("再生（ハイレゾ機能）");
+	}
+	public void stop() {
+		System.out.println("停止（ハイレゾ機能）");
+	}
+	public void next() {
+		System.out.println("次へ（ハイレゾ機能）");
+	}
+	public void back() {
+		System.out.println("戻る（ハイレゾ機能）");
+	}
 	public void call() {
 		System.out.println("電話");
 	}
